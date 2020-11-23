847a1459c5683076aa12a2bb25a6c81a3147fa5e
knoxfighter <knoxfighter@asdff45.de>
2020-11-23 04:57:30 +0100

    debug

--- src/api/mods_handler_test.go
+++ src/api/mods_handler_test.go
@@ -46,7 +46,8 @@ func TestMain(m *testing.M) {
 	}
 	if !load {
 		// no credentials found, login...
-		log.Println(os.Getenv("factorio_username"))
+		getenv := os.Getenv("factorio_password")
+		log.Println(getenv[len(getenv)-5:])
 		err, _ = factorio.FactorioLogin(os.Getenv("factorio_username"), os.Getenv("factorio_password"))
 		if err != nil {
 			log.Printf("Error logging in into factorio: %s", err)
